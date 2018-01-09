307edbc4a177d33f02cb06d2fae52805119b0977
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-09 22:30:52 +0400

    slug

--- src/Http/Controllers/DataTablesConroller.php
+++ src/Http/Controllers/DataTablesConroller.php
@@ -12,7 +12,7 @@ class DataTablesConroller extends Controller
     {
         return DataTables::of(FieldTypes::query())->addColumn('actions', function ($post) {
             $url= url("admin/forms/fields",$post->id);
-            $settings_url= url("admin/forms/type-settings",$post->title);
+            $settings_url= url("admin/forms/type-settings",$post->slug);
             return "<a href='$url' class='btn btn-warning'><i class='fa fa-edit'></i></a>
                     <a href='$settings_url' class='btn btn-primary'><i class='fa fa-eye'></i></a>";
         },2)->addColumn('author', function ($post) {
