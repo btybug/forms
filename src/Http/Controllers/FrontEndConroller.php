cba77d4276fde59589b4002d39397034bc0a6e08
Sahak <hakobyan.sahak88@gmail.com>
2018-01-09 17:13:28 +0400

    test

---
+++ src/Http/Controllers/FrontEndConroller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: menq
+ * Date: 09.01.2018
+ * Time: 16:49
+ */
+
+namespace BtyBugHook\Forms\Http\Controllers;
+
+
+class FrontEndConroller
+{
+
+}
---
+++ src/views/frontend/fields/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: menq
+ * Date: 09.01.2018
+ * Time: 16:54
+ */
