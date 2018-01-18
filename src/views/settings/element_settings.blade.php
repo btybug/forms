afdb77b3cd9f8c66d151841453d6b59142fcaba9
midoalone <mido_alone2001@yahoo.com>
2018-01-19 00:30:46 +0200

    field builder

---
+++ src/views/settings/element_settings.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 1/19/18
+ * Time: 12:13 AM
+ */
---
+++ src/views/settings/form_settings.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 1/18/18
+ * Time: 11:59 PM
+ */
